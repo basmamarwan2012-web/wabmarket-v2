3ab1b6683e71183375c77672f94b34361196efd2
MARWAN <basma.marwan2012@gmail.com>
2026-08-13 23:46:48 +0100

    feat: add portfolio domain profiles and compact inventory

--- app/(dashboard)/admin/domains/[domainId]/edit/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { DomainFormSkeleton } from '@/components/ui/skeletons/domain-form-skeleton'
-
-export default function EditDomainLoading() {
-  return <DomainFormSkeleton />
-}
--- app/(dashboard)/admin/domains/[domainId]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { DomainDetailSkeleton } from '@/components/ui/skeletons/domain-detail-skeleton'
-
-export default function DomainLoading() {
-  return <DomainDetailSkeleton />
-}
